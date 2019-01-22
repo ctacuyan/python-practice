3cc1073366975f2615460e9ddd9718df8795c6e3
Carlo T <46909481+ctacuyan@users.noreply.github.com>
2019-01-21 17:48:27 -0800

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+coordinates=(4,5)
+print("Tuples, unlike lists, are immutable, cannot be altered")
+print(coordinates)
+print(coordinates[0])
+print("Can also make LISTS of Tuples")
+coordinates2=[(4,5),(1,2),(7,8)]
+print(coordinates2)
+print(coordinates2[2])
